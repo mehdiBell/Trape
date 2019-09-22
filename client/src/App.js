38fd91cdb9e9f82500020974a3bea5fe0cfe776f
mehdiBell <mehdi_bellounis@hotmail.fr>
2019-09-22 16:08:40 +0200

    intial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <Fragment>
+      <h2>React app</h2>
+    </Fragment>
+  );
+}
+
+export default App;
